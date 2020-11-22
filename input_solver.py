f1f96474ea5ce7c4cd8cd58960598c7bd804129f
Marco Gellecanao <marcogelle@berkeley.edu>
2020-11-21 19:19:04 -0800

    added comments so eleven can understand

--- input_solver.py
+++ input_solver.py
@@ -6,42 +6,45 @@ from utils import *
 import random
 
 def generate_output(students):
+	print(f'Given {students} students:')
 	k = np.random.randint(1, students)
-	
+
+	# random S_max
+	S_max = np.random.randint(0, 100_000) / 1000
+	print(S_max)
+
+	# make partitions
 	fin = students
 	partitions = []
-	while fin > 0:		
-		iter_size = np.random.randint(1, fin + 1)	
+	while fin > 0:
+		iter_size = np.random.randint(1, fin + 1)
 		fin -= iter_size
 		partitions.append(iter_size)
-
-	
-	S_max = np.random.randint(0, 100000) / 1000
-	print(S_max)
 
 	k = len(partitions)
 	print(partitions)
 
+	# max stress per room
 	k_max = S_max / k
 	print(k_max)
 
-	#
+	# make list of stresses between every student in a partition
 	for p in partitions:
 		s = 0
 		l = []
 		for i in range(p * (p - 1) // 2):
-			n = np.random.randint(0, 100000) / 1000
+			n = np.random.randint(0, 100_000) / 1000 # why this
 			s += n
 			l.append(n)
 		l = [el * k_max / s for el in l]
-		
-		print(l)
-		print(sum(l))	
 
+		print(l) # list of stresses
+		print(sum(l)) # about equal to k_max
+
+	print()
 	return students
 
 def main():
-	print(10)
 	generate_output(10)
 	generate_output(20)
 	generate_output(50)
