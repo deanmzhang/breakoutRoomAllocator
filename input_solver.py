aac46fc3bbbd73c07cc369c433e33d206920140d
Ian <iac>
2020-11-21 17:42:38 -0800

    Created randomized partitioner

--- input_solver.py
+++ input_solver.py
@@ -1,7 +1,26 @@
+#!/usr/bin/env python3
+import numpy as np
+import sys
+
 def generate_output(students):
+	k = np.random.randint(1, students)
+	
+	fin = students
+	partitions = []
+	while fin > 0:		
+		iter_size = np.random.randint(1, fin + 1)	
+		fin -= iter_size
+		partitions.append(iter_size)
+
+	print(partitions)
+
 	return students
 
 def main():
+	print(10)
 	generate_output(10)
 	generate_output(20)
 	generate_output(50)
+
+if __name__ == '__main__':
+	main()
