0d973302ee0a7a22c687384d18f5b5ad07096407
Marco Gellecanao <marcogelle@berkeley.edu>
2020-12-04 17:11:02 -0800

    internal breakout room happiness and stresss levels calculated correctly

--- breakout_room_test.py
+++ breakout_room_test.py
@@ -5,8 +5,9 @@ from solver import BreakoutRoom
 def weight_dict(happiness, stress):
     return {"happiness": happiness, "stress": stress}
 
-def print_two_rooms():
+def two_rooms():
     """Prints the edges dictionary as we do breakout room operations"""
+    print("===TEST: printing edge list to ensure breakout room functionality")
     pp = pprint.PrettyPrinter()
 
     s = 100000000000
@@ -47,8 +48,30 @@ def print_two_rooms():
     assert b0.stress == 11
     assert b1.happiness == 0
     assert b1.stress == 11
-    print("PASSED")
+    print("PASSED\n")
 
+def single_room():
+    """Tests that internal breakout room happiness and stress is calculated correctly"""
+    pp = pprint.PrettyPrinter()
+
+    print("===TEST: testing a single room's happiness and stress is calculated correctly...")
+    s = 100000000000
+    k = 2
+
+    edges = {(0, 1): weight_dict(1, 11),
+            (0, 2): weight_dict(11, 23),
+            (0, 3): weight_dict(94, 11),
+            (1, 2): weight_dict(11, 11),
+            (1, 3): weight_dict(4, 0),
+            (2, 3): weight_dict(0, 11)}
+
+    b0 = BreakoutRoom(0, edges, 0)
+    for i in range(1, 4):
+        b0.add_student(i, s, k)
+    assert b0.happiness == 1 + 11 + 94 + 11 + 4 + 0
+    assert b0.stress == 11 + 23 + 11 + 11 + 0 + 11
+    print("PASSED\n")
 
 if __name__ == "__main__":
-    print_two_rooms()
+    two_rooms()
+    single_room()
