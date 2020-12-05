d26976d904c7a78e5bcac9ccf60a11044f6a38fa
Marco Gellecanao <marcogelle@berkeley.edu>
2020-12-04 16:47:31 -0800

    successfully can add 2 students to singly breakout room

---
+++ breakout_room_test.py
@@ -0,0 +1,47 @@
+import pprint
+from solver import BreakoutRoom
+
+
+def weight_dict(happiness, stress):
+    return {"happiness": happiness, "stress": stress}
+
+def print_two_rooms():
+    """Prints the edges dictionary as we do breakout room operations"""
+    pp = pprint.PrettyPrinter()
+
+    s = 100000000000
+    k = 2
+
+    edges = {(0, 1): weight_dict(1, 11),
+            (0, 2): weight_dict(11, 23),
+            (0, 3): weight_dict(94, 11),
+            (1, 2): weight_dict(11, 11),
+            (1, 3): weight_dict(4, 0),
+            (2, 3): weight_dict(0, 11)}
+    print("Original edges:")
+    pp.pprint(edges)
+    print()
+
+    b0 = BreakoutRoom(0, edges, 0)
+    print("After placing student 0 into breakout 0:")
+    pp.pprint(edges)
+    print()
+
+    b0.add_student(1, s, k)
+    print("After placing student 1 into breakout 0:")
+    pp.pprint(edges)
+    print()
+
+    b1 = BreakoutRoom(1, edges, 2)
+    print("After placing student 2 into breakout 1:")
+    pp.pprint(edges)
+    print()
+
+    b1.add_student(3, s, k)
+    print("After placing student 3 into breakout 1:")
+    pp.pprint(edges)
+    print()
+
+
+if __name__ == "__main__":
+    print_two_rooms()
--- solver.py
+++ solver.py
@@ -9,121 +9,129 @@ import glob
 # 2) If we hit our maximum k, we throw away any edge/pair of students that create a new breakout room
 
 class BreakoutRoom:
-    def __init__(self, id, first_student):
+    def __init__(self, id, edges, first_student):
         self.id = 0 #Counter; increment by 1 every time we create a new BreakoutRoom
         self.stress = 0
         self.happiness = 0
         self.students = set()
+        self.edges = edges
         self.initialize_student(first_student)
 
+    def __repr__(self):
+        """Just for printing/debugging"""
+        return "breakout_{}".format(self.id)
+
     # private method
-    def initialize_student(self, edges, student1):
+    def initialize_student(self, student1):
+        """Initializes breakout room with a single student. Modifies the edges
+        in-place. Returns nothing."""
         # Base Case: Add our first student to the breakout room
         # Still keep stress and happiness to 0
         # Add our first student1 to the student set
         #
-        # Loop through all students/breakout rooms not in this BreakoutRoom, 
+        # Loop through all students/breakout rooms not in this BreakoutRoom,
         # call our current iteration student/breakout room: w
         # Find the edge from (w, student1)/(student1, w)
         # Delete edge from (w, student1)/(student1, w)
         # Add a new edge (w, this breakout room) with the same weight of edge (w, student1)
-        # 
+        #
         # Return new edge list
         self.students.add(student1)
-        for edge, weights in edges.items():
+        for edge in list(self.edges):
+            weights = self.edges[edge]
             u, v = edge[0], edge[1]
             if u == student1:
-                del edges[edge]
+                del self.edges[edge]
                 # possible bug: object isn't hashable
-                edges[(self, v)] = weights
+                self.edges[(self, v)] = weights
             elif v == student1:
-                del edges[edge]
-                edges[(u, self)] = weights
-        return edges
+                del self.edges[edge]
+                self.edges[(u, self)] = weights
 
-    # adds a student to this breakout room; returns new edge list
-    def add_student(self, edges, student, s, k):
-        """Given the edges dictionary, stress budget s, and number of breakout rooms k, adds the specified student to
-        this breakout room."""
+    # Design: should we have s and k as method inputs, or initialize them in the constructor
+    def add_student(self, student, s, k):
+        """Given the edges dictionary, stress budget s, and number of breakout rooms k,
+        adds the specified student this breakout room.
+        Changes the edgeslist in-place, but also returns it."""
         if student in self.students:
             print("This student is already in this breakout room")
             return
-        
+
         # used for calculating the summed phantom edges
         # key: the node connected to this breakout room
         # value: dictionary with "happiness" and "stress"
         new_edges = {}
 
-        for edge in edges:
+        # breakpoint()
+        for edge in list(self.edges):
             u, v = edge[0], edge[1]
-            if type(u) == BreakoutRoom and u.id == self.id:
-                if v == student:
-                    student_found = True
-                    # Initialize variables u=BreakoutRoom and v=student
-                    # Check that this does not exceed our S_max / k. 
-                    # If it does, then we return an Exception saying we exceed S_max / k
-                    #
-                    # Otherwise,
-                    # Add v to the student set (actually, this is done at the end of the function)
-                    # Add v's happiness&stress level to BreakoutRoom's happiness&stress level, 
-                    # according to our edge weight connecting (u, v)
-                    # Delete edge u-v
-                    #
-                    if self.stress + edges[edge]["stress"] > s / k:
-                        # maybe change this to throwing an actual Python exception?
-                        print("Stress budget exceeded in this breakout room")
-                        return
-                    
-                    self.happiness += edges[edge]["happiness"] # kinda forget what get_edge_data dictionary looks like
-                    self.stress += edges[edge]["stress"]
-                    del edges[edge]
+            if (type(u) == BreakoutRoom and u.id == self.id) and (v == student):
+                # Initialize variables u=BreakoutRoom and v=student
+                # Check that this does not exceed our S_max / k.
+                # If it does, then we return an Exception saying we exceed S_max / k
+                #
+                # Otherwise,
+                # Add v to the student set (actually, this is done at the end of the function)
+                # Add v's happiness&stress level to BreakoutRoom's happiness&stress level,
+                # according to our edge weight connecting (u, v)
+                # Delete edge u-v
+                #
+                if self.stress + self.edges[edge]["stress"] > s / k:
+                    # maybe change this to throwing an actual Python exception?
+                    print("Stress budget exceeded in this breakout room")
+                    return
 
-                if v not in self.students:
-                    # Loop through all studentss&breakout rooms not in this BreakoutRoom, 
-                    # call our current iteration student/breakout room: w
-                    # Find edge (w, u)/(u, w) and (w, v)/(v, w)
-                    # WLOG it's in the forward direction: 
-                    # Add the weight of edge w-u and w-v to total weight: tw
-                    # Delete the edge w-u and delete the edge w-v
-                    # Create a brand new edge connecting w-u, with weight tw
-                    # Add this edge
-                    #
-                    # After finishing the loop, we re-sort by our edge ratios
-                    # Return our new edge list
-                    if v not in new_edges:
-                        new_edges[v] = {"happiness": 0, "stress": 0}
-                    old_edge = edges[edge]
-                    new_edges[v]["happiness"] += old_edge["happiness"]
-                    new_edges[v]["stress"] += old_edge["stress"]
-                    del edges[edge]
+                self.happiness += self.edges[edge]["happiness"] # kinda forget what get_edge_data dictionary looks like
+                self.stress += self.edges[edge]["stress"]
+                del self.edges[edge]
 
-            elif type(v) == BreakoutRoom and v.id == self.id:
-                # same code as above, just if u and v are swapped
-                # (there might be a cleaner way to code this rather than this copy and paste shit)
-                if u == student:
-                    if self.stress + edges[edge]["stress"] > s / k:
-                        # maybe change this to throwing an actual Python exception?
-                        print("Stress budget exceeded in this breakout room")
-                        return
-                    
-                    self.happiness += edges[edge]["happiness"] # kinda forget what get_edge_data dictionary looks like
-                    self.stress += edges[edge]["stress"]
-                    del edges[edge]
+            elif (((type(u) == BreakoutRoom and u.id == self.id) or u == student)
+                and v not in self.students):
+                # Loop through all studentss&breakout rooms not in this BreakoutRoom,
+                # call our current iteration student/breakout room: w
+                # Find edge (w, u)/(u, w) and (w, v)/(v, w)
+                # WLOG it's in the forward direction:
+                # Add the weight of edge w-u and w-v to total weight: tw
+                # Delete the edge w-u and delete the edge w-v
+                # Create a brand new edge connecting w-u, with weight tw
+                # Add this edge
+                #
+                # After finishing the loop, we re-sort by our edge ratios
+                # Return our new edge list
+                if v not in new_edges:
+                    new_edges[v] = {"happiness": 0, "stress": 0}
+                old_edge = self.edges[edge]
+                new_edges[v]["happiness"] += old_edge["happiness"]
+                new_edges[v]["stress"] += old_edge["stress"]
+                del self.edges[edge]
 
-                if u not in self.students:
-                    if u not in new_edges:
-                        new_edges[u] = {"happiness": 0, "stress": 0}
-                    old_edge = edges[edge]
-                    new_edges[u]["happiness"] += old_edge["happiness"]
-                    new_edges[u]["stress"] += old_edge["stress"]
-                    del edges[edge]
-                    
+            # The following two elif blocks are the exact same as the above two,
+            # except u and v are swapped. (cleaner way to code this???)
+            elif (type(v) == BreakoutRoom and v.id == self.id) and (u == student):
+                if self.stress + self.edges[edge]["stress"] > s / k:
+                    # maybe change this to throwing an actual Python exception?
+                    print("Stress budget exceeded in this breakout room")
+                    return
+
+                self.happiness += self.edges[edge]["happiness"]
+                self.stress += self.edges[edge]["stress"]
+                del self.edges[edge]
+
+            elif (((type(v) == BreakoutRoom and v.id == self.id) or v == student)
+                and u not in self.students):
+                if u not in new_edges:
+                    new_edges[u] = {"happiness": 0, "stress": 0}
+                old_edge = self.edges[edge]
+                new_edges[u]["happiness"] += old_edge["happiness"]
+                new_edges[u]["stress"] += old_edge["stress"]
+                del self.edges[edge]
+
         self.students.add(student)
-        
+
         for v, weights in new_edges.items():
-            edges[(self, v)] = weights
-        
-        return edges
+            self.edges[(self, v)] = weights
+
+        return self.edges
 
 def solve(G, s):
     """
@@ -141,7 +149,7 @@ def solve(G, s):
     for edge in G.edges:
         u, v = edge[0], edge[1]
         e[(u, v)] = G.get_edge_data(u, v)
-        
+
     # Attempt 1: Choose the edge with highest happiness/stress ratio as first greedy algo
     # Attempt 2: Choose the edge with highest happiness
     # Attempt 3: Use hybrid
@@ -153,18 +161,18 @@ def solve(G, s):
         if stress == 0:
             return float('inf')
         return happy / stress
-        
+
     sorted_edges = sorted(list(G.edges), key=get_ratio, reverse=True)
 
     for ed in list(sorted_edges):
         print(ed, G.get_edge_data(ed[0], ed[1])['happiness'], G.get_edge_data(ed[0], ed[1])['stress'], get_ratio(ed))
-    
+
     # key: breakout room number, value: set of student numbers
     breakouts = {}
     for x in range(0, n/2 + 1):
-        
-    
-    
+        pass
+
+
 
     #print(e)
     return {}, k
