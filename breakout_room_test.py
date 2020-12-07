8e24eb09392cf5a069bedc034003509e661a93c1
Dean Zhang <zhangdean@berkeley.edu>
2020-12-06 18:14:00 -0800

    Buggy Code: Issues are division by 0 and updating edges list so far

--- breakout_room_test.py
+++ breakout_room_test.py
@@ -5,20 +5,21 @@ from solver import BreakoutRoom
 def weight_dict(happiness, stress):
     return {"happiness": happiness, "stress": stress}
 
-def two_rooms():
+def two_rooms_quality_test():
     """Prints the edges dictionary as we do breakout room operations"""
     print("===TEST: printing edge list to ensure breakout room functionality")
     pp = pprint.PrettyPrinter()
 
-    s = 100000000000
+    s = 100
     k = 2
 
-    edges = {(0, 1): weight_dict(1, 11),
-            (0, 2): weight_dict(11, 23),
-            (0, 3): weight_dict(94, 11),
-            (1, 2): weight_dict(11, 11),
-            (1, 3): weight_dict(4, 0),
-            (2, 3): weight_dict(0, 11)}
+    edges = {(0, 1): weight_dict(1, 1),
+            (0, 2): weight_dict(4, 1),
+            (0, 3): weight_dict(6, 1),
+            (1, 2): weight_dict(2, 1),
+            (1, 3): weight_dict(3, 1),
+            (2, 3): weight_dict(5, 1)}
+            
     print("Original edges:")
     pp.pprint(edges)
     print()
@@ -33,21 +34,21 @@ def two_rooms():
     pp.pprint(edges)
     print()
 
-    b1 = BreakoutRoom(1, edges, 2)
-    print("After placing student 2 into breakout 1:")
+    b0.add_student(2, s, k)
+    print("After placing student 2 into breakout 0:")
     pp.pprint(edges)
     print()
 
-    b1.add_student(3, s, k)
+    b1 = BreakoutRoom(1, edges, 3)
     print("After placing student 3 into breakout 1:")
     pp.pprint(edges)
     print()
 
     print("Testing breakout room happiness and stress levels...")
-    assert b0.happiness == 1
-    assert b0.stress == 11
+    assert b0.happiness == 7
+    assert b0.stress == 3
     assert b1.happiness == 0
-    assert b1.stress == 11
+    assert b1.stress == 0
     print("PASSED\n")
 
 def single_room():
@@ -85,6 +86,56 @@ def single_room():
     assert b0.stress == 11 + 23 + 11 + 11 + 0 + 11
     print("PASSED\n")
 
+def test_max_stress_capacity():
+    """Tests that a student will not be added if it would exceed the stress capacity."""
+    """Expected output: Stress exceeded! solver.py line 79. If doesn't print, we have a problem"""
+
+    print("===TEST: printing edge list to ensure breakout room functionality")
+    pp = pprint.PrettyPrinter()
+
+    s = 100
+    k = 2
+
+    edges = {(0, 1): weight_dict(1, 1),
+            (2, 0): weight_dict(4, 69),
+            (3, 0): weight_dict(6, 1),
+            (1, 2): weight_dict(2, 1),
+            (3, 1): weight_dict(3, 1),
+            (2, 3): weight_dict(5, 1)}
+            
+    print("Original edges:")
+    pp.pprint(edges)
+    print()
+
+    b0 = BreakoutRoom(0, edges, 0)
+    print("After placing student 0 into breakout 0:")
+    pp.pprint(edges)
+    print()
+
+    b0.add_student(1, s, k)
+    print("After placing student 1 into breakout 0:")
+    pp.pprint(edges)
+    print()
+
+    b0.add_student(2, s, k)
+    print("After placing student 2 into breakout 0:")
+    pp.pprint(edges)
+    print()
+
+    b1 = BreakoutRoom(1, edges, 3)
+    print("After placing student 3 into breakout 1:")
+    pp.pprint(edges)
+    print()
+
+    print("Testing breakout room happiness and stress levels...")
+    assert b0.happiness == 7
+    assert b0.stress == 71
+    assert b1.happiness == 0
+    assert b1.stress == 0
+    print("FAILED\n")
+    
+
 if __name__ == "__main__":
-    two_rooms()
+    two_rooms_quality_test()
     single_room()
+    test_max_stress_capacity()
--- solver.py
+++ solver.py
@@ -4,9 +4,11 @@ from utils import is_valid_solution, calculate_happiness
 import sys
 from os.path import basename, normpath
 import glob
+import collections
+import pprint
 
-# New ideas: 1) Brute force k, in other words, we increment k if our current k value does not return a valid output
-# 2) If we hit our maximum k, we throw away any edge/pair of students that create a new breakout room
+class CustomError(Exception):
+    pass
 
 class BreakoutRoom:
     def __init__(self, id, edges, first_student):
@@ -37,7 +39,7 @@ class BreakoutRoom:
         #
         # Return new edge list
         self.students.add(student1)
-        for edge in list(self.edges):
+        for edge in list(self.edges.keys()):
             weights = self.edges[edge]
             u, v = edge[0], edge[1]
             if u == student1:
@@ -52,7 +54,7 @@ class BreakoutRoom:
     def add_student(self, student, s, k):
         """Given the edges dictionary, stress budget s, and number of breakout rooms k,
         adds the specified student this breakout room.
-        Changes the edgeslist in-place, but also returns it."""
+        Changes the edges list in-place, but also returns it."""
         if student in self.students:
             print("This student is already in this breakout room")
             return
@@ -62,7 +64,7 @@ class BreakoutRoom:
         # value: dictionary with "happiness" and "stress"
         new_edges = {}
 
-        for edge in list(self.edges):
+        for edge in list(self.edges.keys()):
             u, v = edge[0], edge[1]
             if (type(u) == BreakoutRoom and u.id == self.id) and (v == student):
                 # Initialize variables u=BreakoutRoom and v=student
@@ -74,11 +76,11 @@ class BreakoutRoom:
                 # Add v's happiness&stress level to BreakoutRoom's happiness&stress level,
                 # according to our edge weight connecting (u, v)
                 # Delete edge u-v
-                #
                 if self.stress + self.edges[edge]["stress"] > s / k:
                     # maybe change this to throwing an actual Python exception?
-                    print("Stress budget exceeded in this breakout room")
-                    return
+                    #print("Stress budget exceeded in this breakout room")
+                    raise CustomError("Stress budget exceeded in this breakout room")
+                    #return
 
                 self.happiness += self.edges[edge]["happiness"] # kinda forget what get_edge_data dictionary looks like
                 self.stress += self.edges[edge]["stress"]
@@ -109,8 +111,9 @@ class BreakoutRoom:
             elif (type(v) == BreakoutRoom and v.id == self.id) and (u == student):
                 if self.stress + self.edges[edge]["stress"] > s / k:
                     # maybe change this to throwing an actual Python exception?
-                    print("Stress budget exceeded in this breakout room")
-                    return
+                    #print("Stress budget exceeded in this breakout room")
+                    #return
+                    raise CustomError("Stress budget exceeded in this breakout room")
 
                 self.happiness += self.edges[edge]["happiness"]
                 self.stress += self.edges[edge]["stress"]
@@ -147,7 +150,9 @@ def solve(G, s):
     n = len(G.nodes)
     e = {}
     k = 0
-    e_tuples = []
+    d = {}
+    pp = pprint.PrettyPrinter()
+
     for edge in G.edges:
         u, v = edge[0], edge[1]
         e[(u, v)] = G.get_edge_data(u, v)
@@ -156,28 +161,120 @@ def solve(G, s):
     # Attempt 2: Choose the edge with highest happiness
     # Attempt 3: Use hybrid
 
-    # this is attempt 1
-    def get_ratio(edge):
+    def get_ratio_initial(edge):
         happy = G.get_edge_data(edge[0], edge[1])['happiness']
         stress = G.get_edge_data(edge[0], edge[1])['stress']
         if stress == 0:
             return float('inf')
         return happy / stress
 
-    sorted_edges = sorted(list(G.edges), key=get_ratio, reverse=True)
+    def get_ratio(edge):
+        happiness = e[(edge[0], edge[1])]['happiness']
+        stress = e[(edge[0], edge[1])]['stress']
+        if stress == 0:
+            return float('inf')
+        return happiness/stress
 
-    for ed in list(sorted_edges):
+    """def get_happiness(edge):
+        happiness = G.get_edge_data(edge[0], edge[1])['happiness']
+        return happiness
+    
+    def get_stress(edge):
+        stress = G.get_edge_data(edge[0], edge[1])['stress']
+        return stress"""
+
+    sorted_edges = sorted(list(G.edges), key=get_ratio_initial, reverse=True)
+    
+    # Pseudocode
+    # Outer Loop O: We want to loop from k = 0 to k = n, where k is our current maximum allowed breakout rooms
+    # Initialize a counter, called "current_k". This checks the current amount of breakout rooms we've created
+    
+    # Take our edges dictionary, and sort in descending order of happiness : stress ratio
+    # Call this edge we took out of the edge dictionary: highest_ratio_edge
+    # First, check if we have met our capacity. In other words, if current_k == k, then we cannot create any more breakout rooms!
+    
+    # If current_k < k, then if highest_ratio_edge connects two students, then form a new breakout room with these new students.
+    # Else if current_k == k OR s_max/k is exceeded, then we throw out highest_ratio_edge, because we cannot form any more breakout rooms if we have met our capacity of k rooms already created
+    
+    # If highest_ratio_edge contains a breakout_room and a student, then we can simply add this student to the breakout room IF s_max/k isn't exceeded
+    # Else, if S_max/k is exceeded, throw out highest_ratio_edge
+
+    # If highest_ratio_edge contains a breakout_room and a breakout_room, throw this edge out for now because this is undefined behavior. We may need to implement this later if it improves our alg.
+    
+    # Now, we re-sort the edges dictionary again in descending order of happiness : stress ratio
+    # Continue this process until:
+    # a) all edges are thrown away. This means that there is no valid solution for these amount of k rooms, so we must increase k = k + 1 and repeat
+    # b) all students have been successfully assigned to a breakout room with k possible rooms. This is good! We can output this solution as our answer
+
+    # End Outer Loop O
+
+    while k <= n:
+        current_k = 0
+
+        sorted_edges = sorted(e.keys(), key=get_ratio, reverse=True)
+        highest_ratio_edge = sorted_edges[0]
+        
+        if current_k < k:
+            u, v = highest_ratio_edge[0], highest_ratio_edge[1]
+            if type(u) == int and type(v) == int:
+                # Preemptive thought: sorted_e might need the highest_ratio_edge removed before proceeding
+                try:
+                    newBreakoutRoom = BreakoutRoom(k, e, u)
+                    e = newBreakoutRoom.add_student(v, s, k)
+                    d[u] = k
+                    d[v] = k
+                    current_k += 1
+                except CustomError: #CustomError is raised when s_max/k limit is violated
+                    del e[highest_ratio_edge] # deleting highest_ratio_edge
+            if (type(u) == BreakoutRoom and type(v) == int) or (type(u) == int and type(v) == BreakoutRoom):
+                try:
+                    if type(u) == BreakoutRoom:
+                        e = u.add_student(v, s, k) #if this doesn't work, use k
+                        d[v] = current_k
+                    if type(v) == BreakoutRoom:
+                        e = v.add_student(u, s, k)
+                        d[u] = current_k
+                except CustomError: #CustomError is raised when s_max/k limit is violated
+                    del e[highest_ratio_edge]
+            if type(u) == BreakoutRoom and type(v) == BreakoutRoom:
+                # For now, we're gonna throw out highest_ratio_edge b/c this is undefined behavior. 
+                # Might keep highest_ratio_edge later on if this improves our algo
+                del e[highest_ratio_edge]
+        
+        if current_k >= k:
+            del e[highest_ratio_edge]
+
+        if len(e) == 0:
+            sorted_edges = sorted(e.keys(), key=get_ratio, reverse=True)
+            k += 1
+        else:
+            # d is sorted by students with highest happiness/stress ratios
+            # D is final output, where D is sorted by ascending student number (by keys)
+            D = {}
+            for student in sorted(d.keys()):
+                D[student] = d[student]
+            return D, k
+            
+        
+        
+
+    
+    """for ed in list(sorted_edges):
         print(ed, G.get_edge_data(ed[0], ed[1])['happiness'], G.get_edge_data(ed[0], ed[1])['stress'], get_ratio(ed))
+        print(sorted_edges)
+        pp.pprint(e)
+        print("***")"""
+    
 
     # key: breakout room number, value: set of student numbers
+    # New ideas: 1) Brute force k, in other words, we increment k if our current k value does not return a valid output
+    # 2) If we hit our maximum k, we throw away any edge/pair of students that create a new breakout room
+
+    """
     breakouts = {}
     for x in range(0, n/2 + 1):
         pass
-
-
-
-    #print(e)
-    return {}, k
+    """
 
 
 # Here's an example of how to run your solver.
